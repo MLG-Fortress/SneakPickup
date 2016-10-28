31beca791e0fc6f2e80d234aaf8dc31d332c2122
RoboMWM <robomwm@users.noreply.github.com>
2016-10-28 16:50:08 -0700

    ignore arrow pickups

--- src/main/java/to/us/tf/SneakPickup/SneakPickup.java
+++ src/main/java/to/us/tf/SneakPickup/SneakPickup.java
@@ -11,6 +11,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerPickupArrowEvent;
 import org.bukkit.event.player.PlayerPickupItemEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -156,6 +157,10 @@ public class SneakPickup extends JavaPlugin implements Listener
 
         //Ignore players who have disabled SneakPickup
         if (storage.get(player.getUniqueId().toString()) != null)
+            return;
+
+        //Ignore arrow pickups
+        if (event instanceof PlayerPickupArrowEvent)
             return;
 
         if (!remindedThisSession.contains(event.getPlayer()))
